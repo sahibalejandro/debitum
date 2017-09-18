b0ebf40cad68d0c8b8490f09620c7aabfa0d30ed
Sahib J. Leo <sahib.alejandro@gmail.com>
2017-09-18 00:56:59 -0500

    Fix "sign in with google" image sizing.

--- resources/assets/js/components/login.vue
+++ resources/assets/js/components/login.vue
@@ -7,7 +7,7 @@
         <div data-turbolinks="false">
             <div class="text-center mt-5">
                 <a href="/login/redirect/google">
-                    <img src="/i/btn_google_signin_dark_normal_web@2x.png" alt="Sign in with Google">
+                    <img class="img-fluid" src="/i/btn_google_signin_dark_normal_web@2x.png" alt="Sign in with Google">
                 </a>
             </div>
         </div>
